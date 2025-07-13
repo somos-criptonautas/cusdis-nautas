2ca0d1ef0f8ef83fb96ea1177c863c6306fc72ed
satoshinotdead <dev@criptonautas.co>
2025-07-13 14:39:20 -0500

    previous

--- widget/components/Reply.svelte
+++ widget/components/Reply.svelte
@@ -69,6 +69,16 @@
         bind:value={nickname}
       />
     </div>
+    <div class="px-1">
+      <label class="mb-2 block dark:text-gray-200" for="email">{t('email')}</label>
+      <input
+        name="email"
+        class="w-full p-2 border border-gray-200 bg-transparent  dark:text-gray-100 dark:outline-none"
+        type="email"
+        title={t('email')}
+        bind:value={email}
+      />
+    </div>
   </div>
 
   <div class="px-1">
